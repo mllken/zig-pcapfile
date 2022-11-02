9a5698187a694bc809b6cccec58a9365757c504a
unknown <you@example.com>
2022-11-02 01:32:09 -0400

    comment out pcapng

--- pcapfile.zig
+++ pcapfile.zig
@@ -1,7 +1,7 @@
 // Legacy Pcap file format
 pub const pcap = @import("pcap.zig");
 // Pcap NG
-pub const pcapng = @import("pcapng.zig");
+//pub const pcapng = @import("pcapng.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
